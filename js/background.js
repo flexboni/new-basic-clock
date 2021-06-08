aeab8777301003cda23bc2cd3c7812e822b81cd1
flexboni <kgb8594@gmail.com>
2021-06-08 17:05:38 +0900

    11.2 Deleting To Dos

--- js/background.js
+++ js/background.js
@@ -1,5 +1,4 @@
 const images = [
-    "0.jpeg",
     "1.jpeg",
     "2.jpeg",
     "3.jpeg",
--- js/todo.js
+++ js/todo.js
@@ -2,19 +2,28 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newToDo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerHTML = newToDo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
-    paintToDo(newToDo);
     toDoInput.value = "";
+    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
