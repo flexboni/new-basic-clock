4e64d53ce617b8df242bb8919531d8ea158844ba
flexboni <kgb8594@gmail.com>
2021-06-08 18:05:15 +0900

    12.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = "";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
     console.log("You live in ", lat, lng);
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
 
 function onGeoError() {
